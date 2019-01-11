7653d486a2de51b569e24fd500517cdb6af3bb72
cynkix <cyril.renault@zenika.com>
2019-01-11 14:45:45 +0100

    Create monjs.js

---
+++ src/monjs.js
@@ -0,0 +1,32 @@
+<script type="text/javascript">
+
+
+let design = {
+              fatec: {
+                     name: 'AYA',
+                     annee: 2018,
+                     designer: 'cyril - cecile',
+                     codeur: 'florent',
+                     client: 'Fatec'
+                     },
+              partnaire: {
+                     name: 'ZENIKA Paris',
+                     annee: 2018,
+                     designer: 'cyril',
+                     codeur: 'damien',
+                     client: 'Partnaire'
+                     },
+            zenikalabs: {
+                     name: 'ZENIKA labs',
+                     annee: 2018,
+                     designer: 'andres',
+                     codeur: 'romain',
+                     client: 'BNP'
+                     }
+                     
+ 
+ console.log(design.fatec.name);
+ 
+       
+
+</script>
