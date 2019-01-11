cac4ff666efc38fe28fb842a66ce22a2a4c87378
cynkix <cyril.renault@zenika.com>
2019-01-11 17:40:03 +0100

    Update monjs.js

--- src/monjs.js
+++ src/monjs.js
@@ -1,4 +1,4 @@
-<script type="text/javascript">
+ 
 
 
 let design = {
@@ -29,5 +29,20 @@ let design = {
  console.log(design.fatec.name);
  
        
+let button = document.querySeletion('button');
+let nbLike=0;
+button.addListener(click,()=> 
+{  
+  button.style.animation = 'blink 0.2s linear 5 alternate';  
+  ndLike++;
+  if(nbLike>1){
+    button.innerHTML=nbLike + 'likes';
+  }else {
+    button.innerHTML=nbLike + ' do you like?';
+  }
+});
 
-</script>
+console.log(UUUUU);
+button.addListener('animationned',() => {
+ button.style.animation = 'none';
+         });
